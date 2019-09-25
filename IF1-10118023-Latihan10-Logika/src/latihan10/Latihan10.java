d488dccdf184a025c3e4bd42ec0028817c1279cf
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2019-09-25 14:50:52 +0700

    Add files via upload

---
+++ IF1-10118023-Latihan10-Logika/src/latihan10/Latihan10.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan10;
+
+/**
+ * @author Zaenal PC
+ * NAMA              : Zaenal Anzarry
+ * KELAS             : IF-01
+ * NIM               : 10118023
+ * Deskripsi Program : Logika
+ */
+
+public class Latihan10 {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur>= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
